52b6a2369840b352d3078ea4ebac19c5dec84444
KushalBiyani <KushalBiyani@gmail.com>
2020-12-28 15:55:09 +0530

    Deploying client on heroku part-0

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
-const port = process.env.PORT || 3001;
+const port = process.env.PORT || 3000;
 const database = require('./database');
 const bodyParser = require('body-parser');
 const path = require('path');
@@ -12,20 +12,13 @@ app.use(cors());
 
 // route
 app.use(bodyParser.json());
-app.use(express.static(path.join(__dirname, 'client/build')));
+app.use(express.static(path.join(__dirname, 'login_client/build')));
 app.use('/api', authRoutes);
-
+app.get('*', function(req, res) {
+  res.sendFile(path.join(__dirname, 'login_client/build', 'index.html'));
+});
 
 // Start Server
 app.listen(port, () => {
   console.log("Server running at port : " + port);
 });
-// {
-//     "firstName": "kushal",
-//     "lastName":"biyani",
-//     "email" : "kuslalbiyani@gmail.com",
-//     "password":"12345684",
-//     "gender":"male",
-//     "dateOfBirth":"03/14/2000"
-    
-//   }
