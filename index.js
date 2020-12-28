383596709665fcbe25da04ae5b02652acf7acdc9
KushalBiyani <KushalBiyani@gmail.com>
2020-12-28 16:11:51 +0530

    Deploying client on heroku part-1

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const port = process.env.PORT || 3000;
-const database = require('./database');
+const database = require('./src/database');
 const bodyParser = require('body-parser');
 const path = require('path');
 
-const authRoutes = require("./routes/auth");
+const authRoutes = require("./src/routes/auth");
 
 app.use(cors());
 
@@ -14,7 +14,7 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, 'login_client/build')));
 app.use('/api', authRoutes);
-app.get('*', function(req, res) {
+app.get('/*', function(req, res) {
   res.sendFile(path.join(__dirname, 'login_client/build', 'index.html'));
 });
 
