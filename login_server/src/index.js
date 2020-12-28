25ca499250bef2bcc60cd81625a9b90f6d0c777c
KushalBiyani <KushalBiyani@gmail.com>
2020-12-28 15:33:16 +0530

    heroku

--- login_server/src/index.js
+++ login_server/src/index.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 const cors = require("cors");
-const port = process.env.PORT;
+const port = process.env.PORT || 3001;
 const database = require('./database');
 const bodyParser = require('body-parser');
 const path = require('path');
